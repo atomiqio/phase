b1aa62ba3ca289d74817a53ea466d2e403965b7b
Tony Pujals <tonypujals@users.noreply.github.com>
2015-07-02 17:40:07 -0700

    added doc comments to test.js

--- src/test/test.js
+++ src/test/test.js
@@ -1,27 +1,25 @@
-import promisify from '@atomiq/promisify';
 import assert from 'assert';
 import { join, dirname } from 'path';
 import { readdirSync, readFileSync, writeFileSync } from 'fs';
-import { Phase } from '../lib/phase';
-import { Phase6 } from '../phase6';
-import tv4 from 'tv4';
-import ZSchema from 'z-schema';
-import testSuite from './testsuite';
-import { draft4 } from 'json-schema-test-suite';
 import { format, inherits } from 'util';
 import { isEqual } from 'lodash';
+import tv4 from 'tv4';
+import ZSchema from 'z-schema';
+import { Phase } from '../lib/phase';
+import testSuite from './testsuite';
+import { draft4 } from 'json-schema-test-suite';
 
 const prn = console.log;
 
+// The testsuite directory structure was created using the gulp task generate-testsuite.
+// It uses the testsuite module's exported generate function to create and populate
+// directories with schema.json and tests.json files. These files are based on tests exported
+// by json-schema-test-suite. The loadSamples function in this module relies on this
+// directory structure. The tests will load a schema.phase file, if present, to compare
+// against the reference schema.json file.
 const testSuitePath = join(__dirname, './testsuite');
 
-// As we process both phase and phase6 schemas, we will want to compare against canonical
-// JSON Schemas. We will also want to ensure that the sample JSON files we use for testing
-// phase/phase6 schemas actually pass canonical JSON Schema tests (thus ensuring we are
-// actually using good test samples).
-//
-// For testing, will compare with both tv4 and z-schema (which swagger uses)
-
+// factory function for creating a tv4 JSON Schema validator
 const tv4SchemaFactory = (schema, options) => {
   if (typeof schema == 'string') {
     schema = JSON.parse(schema);
@@ -39,6 +37,7 @@ const tv4SchemaFactory = (schema, options) => {
   }
 };
 
+// factory function for creating a z-schema JSON Schema validator
 const zSchemaFactory = (schema, options) => {
   const zschema = new ZSchema(options);
 
@@ -58,30 +57,43 @@ const zSchemaFactory = (schema, options) => {
   };
 };
 
-// for a factory, schema is either a string or JSON object, and options is a validator-specific set of options to pass along
 const validators = [
 //  { name: 'tv4-schema', factory: (schema, options) => tv4SchemaFactory(schema, options), ext: '.json', skip: process.env.SKIP_TV4_SCHEMA },
 //  { name: 'z-schema', factory: (schema, options) => zSchemaFactory(schema, options), ext: '.json', skip: process.env.SKIP_Z_SCHEMA },
   { name: 'phase', factory: (schema, options) => { return Phase.parse(schema, options) }, ext: '.phase', skip: process.env.SKIP_PHASE }
-//  { name: 'phase6', factory: (schema, options) => { return new Phase6(schema, options); }, ext: '.phase6', skip: process.env.SKIP_PHASE6 }
 ];
 
 
 /**
- * Traverse the testsuite directory and yield schema objects:
+ * Generator function to traverse the testsuite directory and yield schema sample objects:
+ * 
+ * Directory structure:
+ *
+ * testsuite/
+ *   additionalItems/
+ *     additionalItems~are~allowed~by~default/
+ *       schema.json
+ *       schema.phase
+ *       tests.json
+ *   ...
+ * 
+ * Generates schema samples:
+ * 
  * {
- *   group: '',
- *   schema: '',
- *   description: '',
- *   filename: '',
- *   filepath: '',
+ *   group: 'additionalItems',
+ *   schema: '(contents of schema.json)',
+ *   description: 'additionalItems are allowed by default',
+ *   filename: 'schema.phase',
+ *   filepath: '/.../testsuite/additionalItems/additionalItems~are~allowed~by~default/schema.phase',
  *   tests: [{
  *     description: '',
  *     data: *,
  *     valid: t|f
- *   }]
+ *   }, ...]
  * }
  *
+ * @param {string} ext - File extension to load, such as '.phase'
+ * @return {object} yields a schema sample
  */
 function* loadSamples(ext) {
   const testsuitePath = join(__dirname, 'testsuite');
@@ -95,25 +107,17 @@ function* loadSamples(ext) {
 
 describe('validator tests', function () {
 
-  after(() => {
-    //prn('===================');
-    //prn('Summary');
-    //prn('===================');
-  });
-
   for (let { name, factory, ext, skip } of validators) {
     if (skip) continue;
 
+    // create a test suite for each validator under test
     describe(name, () => {
-      before(() => {
-        // just to make the test suites a little easier to distinguish in the console
-        //prn('===================');
-      });
-
-
       for (const sample of loadSamples(ext)) {
 
+        // create a test suite for each sample group
         describe(sample.group, function () {
+
+	  // create a test suite for sample group description
           describe(sample.description, () => {
 
 	    try {
@@ -122,15 +126,20 @@ describe('validator tests', function () {
 	      dumpDebug(dirname(sample.filepath), phaser);
 
 	      for (const test of sample.tests) {
+
+	        // create a test case for each test
 		it(test.description, function () {
+
 		  // if the generated schema is equal to the source, then validation should pass
 		  verifyEqual(phaser, sample);
 
 		  // explicit validation
 		  //validate(phaser, sample, test);
+
 		});
 	      }
 	    } catch (err) {
+	      // SyntaxError requires special handling with Mocha
 	      if (err.name == 'SyntaxError') {
 		console.log(err);
               }
@@ -145,18 +154,25 @@ describe('validator tests', function () {
 
 });
 
+/**
+ * To help with debugging, dump a file with the parsed AST and generated schema
+ * @param {string} dir - The directory representing the current test case
+ * @param {object} validator - Phase instance
+ */
 function dumpDebug(dir, validator) {
   if (validator.ast && validator.schema) {
     let data = {
       ast: validator.ast,
       schema: validator.schema
     }
-
     writeFileSync(join(dir, 'dump.json'), JSON.stringify(data, null, 2));
   }
 }
 
-function dump(msg, validator, sample, test, result) {
+/**
+ * Helper function for ValidationError
+ */
+function dumpStr(msg, validator, sample, test, result) {
   let lines = [];
 
   function prn() {
@@ -183,7 +199,9 @@ function dump(msg, validator, sample, test, result) {
   return lines.join('\n');
 }
 
-
+/**
+ * Provide better error info than AssertionError when comparing validation result against expected
+ */
 function ValidationError(validator, sample, test, result) {
   this.name = 'ValidationError';
 
@@ -191,9 +209,9 @@ function ValidationError(validator, sample, test, result) {
 
   // it should have passed if test.valid is true
   if (test.valid && !result.valid) {
-    message = dump('expected to pass', validator, sample, test, result);
+    message = dumpStr('expected to pass', validator, sample, test, result);
   } else {
-    message = dump('expected to fail', validator, sample, test);
+    message = dumpStr('expected to fail', validator, sample, test);
   }
 
   this.message = message;
@@ -201,6 +219,9 @@ function ValidationError(validator, sample, test, result) {
 
 inherits(ValidationError, Error);
 
+/**
+ * Validate the test data using the generated schema
+ */
 function validate(validator, sample, test) {
   const result = validator.validate(test.data);
 
@@ -209,6 +230,9 @@ function validate(validator, sample, test) {
   }
 }
 
+/**
+ * Provide better error info than AssertionError when comparing generated schema against test suite schema
+ */
 function VerificationError(validator, sample) {
   this.name = 'VerificationError';
   this.message = dump('generated schema does not match original', validator, sample);
@@ -216,6 +240,9 @@ function VerificationError(validator, sample) {
 
 inherits(VerificationError, Error);
 
+/**
+ * Verify that the generated schema is identical to the test suite schema
+ */
 function verifyEqual(validator, sample) {
   if (!isEqual(validator.schema, sample.jsonSchema)) {
     throw new VerificationError(validator, sample);
